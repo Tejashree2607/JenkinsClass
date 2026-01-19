556eaa5135e91cf4c37280e5394c2f074e8cf263
Tejashree2607 <tejashreegurav01@gmail.com>
2026-01-20 01:44:40 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java!"); 
+    //Call a method to add two numbers
+      int result = addNumbers(10,20);
+      System.out.println("The Sum is: " +result);
+      
+    }
+  public class int addNumbers(int a, int b)
+  {
+    return a + b;
+  }
+  
+}
